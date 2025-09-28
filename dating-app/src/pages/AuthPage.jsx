8fdc9f4ac68e79784de550da26c06b8c2c4161f6
Jeremy <jrjn71780@gmail.com>
2025-09-28 19:17:44 +0300

    changes

--- dating-app/src/pages/AuthPage.jsx
+++ dating-app/src/pages/AuthPage.jsx
@@ -17,7 +17,10 @@ export default function AuthPage({ onLogin }) {
           backgroundColor: "#fff",
           borderBottom: "1px solid #eee",
         }}
-      >
+      > 
+        <div style={{ height: "1px" ,}}>
+          <h5 style={{ fontSize: "10px" , marginBottom: "15px"   }}>HeartLink</h5>
+          </div>
         <div
           style={{
             background: "#fff",
@@ -36,13 +39,41 @@ export default function AuthPage({ onLogin }) {
           backgroundImage: "url('/sajad-nazeran-g3Wi7ud9IZA-unsplash.jpg')",
           backgroundSize: "cover",
           backgroundPosition: "center",
-          width: "100%",
-          minHeight: "300px",
+          width: "96%",
+          minHeight: "750px",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
+        {/* Left: Hero Text */}
+        {/* Left: Hero Text */}
+      <div
+        style={{
+          flex: 1,
+          color: "#fff",
+          maxWidth: "500px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          height: "100%",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "60px",
+            lineHeight: "1.1",
+            margin: 0,
+            whiteSpace: "pre-line", // important
+          }}
+        >
+          Get Who{'\n'}Gets You
+        </h1>
+      </div>
+
+
+
+        {/* Signup form container */}
         <div
           style={{
             background: "rgba(255,255,255,0.9)",
@@ -51,6 +82,8 @@ export default function AuthPage({ onLogin }) {
             boxShadow: "0 2px 6px rgba(0,0,0,0.1)",
             maxWidth: "400px",
             width: "100%",
+            marginRight: "800px",
+            marginTop: "200px"
           }}
         >
           <h3 style={{ marginTop: 0, textAlign: "center" }}>Sign Up</h3>
@@ -59,7 +92,7 @@ export default function AuthPage({ onLogin }) {
       </div>
 
       {/* 3-column TEXTS below */}
-      <div style={{ display: "flex", width: "100%", minHeight: "100px" }}>
+      <div style={{ display: "flex", width: "96%", minHeight: "250px" }}>
         <div
           style={{
             flex: 1,
@@ -69,9 +102,10 @@ export default function AuthPage({ onLogin }) {
             fontSize: "1.2rem",
             fontWeight: "bold",
             background: "#f2f2f2",
+            
           }}
         >
-          the first is good
+          The 1# Trusted Dating App
         </div>
         <div
           style={{
@@ -84,7 +118,7 @@ export default function AuthPage({ onLogin }) {
             background: "#e6e6e6",
           }}
         >
-          the third is good
+          Get a Date Within 10Minutes
         </div>
         <div
           style={{
@@ -97,18 +131,22 @@ export default function AuthPage({ onLogin }) {
             background: "#d9d9d9",
           }}
         >
-          how good are u
+          Match and Find Love
         </div>
+      </div>
+      <div style={{ textAlign: "center" , minHeight: "120px" , justifyContent: "center"}}>
+        <h1 style={{ fontSize:"30px" , justifyContent: "center" ,color:"red", marginTop:"50px"}}>Our App Helps You Find True Love In The World Where Love Died</h1>
       </div>
 
       {/* 3-column IMAGES */}
-      <div style={{ display: "flex", width: "100%", minHeight: "250px" }}>
+      <div style={{ display: "flex", width: "96%", minHeight: "350px" }}>
         <div
           style={{
             flex: 1,
             backgroundImage: "url('/freddie-addery-UlcD_8t_zek-unsplash.jpg')",
             backgroundSize: "cover",
             backgroundPosition: "center",
+            borderRadius: "70px"
           }}
         />
         <div
@@ -117,6 +155,7 @@ export default function AuthPage({ onLogin }) {
             backgroundImage: "url('/dmitry-ganin-Qq_EbjdTkzo-unsplash (1).jpg')",
             backgroundSize: "cover",
             backgroundPosition: "center",
+            borderRadius: "70px"
           }}
         />
         <div
@@ -125,6 +164,7 @@ export default function AuthPage({ onLogin }) {
             backgroundImage: "url('/allef-vinicius-pOrrjxBo6i4-unsplash (1).jpg')",
             backgroundSize: "cover",
             backgroundPosition: "center",
+            borderRadius: "70px"
           }}
         />
       </div>
@@ -141,7 +181,7 @@ export default function AuthPage({ onLogin }) {
             background: "#f2f2f2",
           }}
         >
-          how are u
+          Millions Have Find Love
         </div>
         <div
           style={{
@@ -153,7 +193,7 @@ export default function AuthPage({ onLogin }) {
             background: "#e6e6e6",
           }}
         >
-          wat a uddcscb
+          Find Who Truly Gets You
         </div>
         <div
           style={{
@@ -165,7 +205,7 @@ export default function AuthPage({ onLogin }) {
             background: "#d9d9d9",
           }}
         >
-          feydfa dh sda
+          Love That Lasts Forever
         </div>
       </div>
 
